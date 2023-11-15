3205520b6ad94a0b018f6c4dda12ad39a33b820f
Nathan Ojuolape <nathan.ojuolape@diageo.com>
2023-11-15 00:44:09 +0000

    The initial push of basic playwright practices

---
+++ login.test.ts
@@ -0,0 +1,39 @@
+import {expect, test} from '@playwright/test';
+
+const expectedUrl = 'https://practicetestautomation.com/practice-test-login/';
+// const loggedinUrl = 'https://practicetestautomation.com/logged-in-successfully/';
+
+test('Navigating to the page', async ({ page }) => {
+
+    await page.goto(expectedUrl);
+
+//    Get the current URL
+    const currentUrl = page.url();
+// Assert that the current URL matches the expected URL
+    console.assert(currentUrl === expectedUrl, `Expected URL: ${expectedUrl}, Actual URL: ${currentUrl}`);
+});
+
+test('Logging in to the page', async ({ page }) => {
+    const username = 'student';
+    const password = 'Password123';
+    // Go to the website
+    await page.goto(expectedUrl);
+
+    // Fill in the username and password fields and click the submit button
+    await page.fill('input[name="username"]', username);
+    await page.fill('input[name="password"]', password);
+    await page.click('button#submit.btn');
+});
+
+// test('Verify that the login is successful', async ({page}) => {
+    // Verify "Logged In Successfully" text
+    // await page.waitForSelector('text=Logged In Successfully');
+    //
+    // // Verify the URL
+    // const currentUrl = page.url();
+    // const loggedinUrl = 'https://practicetestautomation.com/logged-in-successfully/';
+    // expect(currentUrl).toBe(loggedinUrl);
+    //
+    // console.log('Login successful and URL verified.');
+
+// });
---
+++ login2.test.ts
@@ -0,0 +1,35 @@
+// import { test } from '@playwright/test';
+// import {Protocol} from "playwright-core/types/protocol";
+// import Page = Protocol.Page;
+//
+// test('Navigating in to the website', async ({ page }) => {
+//     // Your test code here
+//
+//     const expectedUrl = 'https://practicetestautomation.com/practice-test-login/';
+//
+// // Navigate to the page
+//     await page.goto(expectedUrl);
+//
+// // Get the current URL
+//     const currentUrl = page.url();
+//
+// // Assert that the current URL matches the expected URL
+//     console.assert(currentUrl === expectedUrl, `Expected URL: ${expectedUrl}, Actual URL: ${currentUrl}`);
+//
+// });
+// //
+// //
+// // test('Logging in to the page', async ({ page }) => {
+// //     const username = 'student'; // Replace with the actual username
+// //     const password = 'Password123'; // Replace with the actual password
+// //
+// //     // Fill in the username and password fields and click the submit button
+// //     await page.fill('input[name="username"]', username);
+// //     await page.fill('input[name="password"]', password);
+// //     await page.click('button#submit.btn');
+// // });
+// //
+// //
+// //
+// //
+// //
